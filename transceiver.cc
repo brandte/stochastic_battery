6eaffb7d28bde9aa60317bba7be36388efb34496
Flosch94 <Flosch@eduroam-pool12-1235.wlan.uni-bremen.de>
2016-12-12 14:19:59 +0100

    Step successfull: Implementing all underlying sample radio power consumptions for simulation. Transeiver has now specific radio frequencies and the measurement values.

--- transceiver.cc
+++ transceiver.cc
@@ -7,6 +7,7 @@ class transceiver : public cSimpleModule
 {
   private:
     //float power_lvl;    //The power level of the transceiver that will later be send to the battery.
+    int power_consumption;           //Power level of the Batter in micro-Ampere
     cMessage *measuring_interval;    //This one is needed for the sender, to set the measure and send interval.
   protected:
     virtual void initialize() override;
@@ -16,11 +17,10 @@ class transceiver : public cSimpleModule
 
 Define_Module(transceiver);
 
-void transceiver::initialize()
-{
+void transceiver::initialize(){
     if (strcmp("Sender", getName()) == 0) {
         send (new cMessage(),"gate$o");
-       power
+        power_level("send");
         measuring_interval = new cMessage("measuring interval");
         scheduleAt(normal(1800,60),measuring_interval);
     }
@@ -33,19 +33,65 @@ void transceiver::handleMessage(cMessage *msg)
         send (new cMessage(),"gate$o");
         power_level("send");
         scheduleAt(simTime()+normal(1800,60),measuring_interval);
-    }else{      //dann sind wir der Empf�nger und haben eine Nachricht von Au�en bekommen
-        //hier haben wir jetzt eine Nachricht empfangen und m�ssen unseren Pegel dementsprechend anpassen.
-        //sp�ter...
-        bubble("Hurray");
+    }else{
+
     }
 }
 
 void transceiver::power_level(std::string activity)
 {
-    if (strcmp("CC2530", par("transceiver_type")) ==0){
-        if (strcmp(){
+    if (strcmp(par("transceiver_type"),("CC2530"))==0){
+        if (activity=="send"){
+            power_consumption=33500;
+            bubble("Sending");
 
+        }else if(activity=="receive"){
+            power_consumption=24300;
+            bubble("Receiving");
+        }else{      //if state("sleeping")
+            power_consumption=200;
+            bubble("sleeping/idle");
         }
+
+    }else if (strcmp(par("transceiver_type"),("ESP8266"))==0){
+        if (activity=="send"){
+            power_consumption=215000;
+            bubble("Sending");
+
+        }else if(activity=="receive"){
+            power_consumption=60000;
+            bubble("Receiving");
+        }else{      //if state("sleeping")
+            power_consumption=10;
+            bubble("sleeping/idle");
+        }
+
+    }else if(strcmp(par("transceiver_type"),("CC2650"))==0){
+        if (activity=="send"){
+                  power_consumption=9100;
+              bubble("Sending");
+
+          }else if(activity=="receive"){
+              power_consumption=6100;
+              bubble("Receiving");
+          }else{      //if state("sleeping")
+              power_consumption=1;
+              bubble("sleeping/idle");
+          }
+
+    }else if(strcmp(par("transceiver_type"),("RFD22301"))==0){
+        if (activity=="send"){
+               power_consumption=12000;
+               bubble("Sending");
+
+           }else if(activity=="receive"){
+               power_consumption=12000;
+               bubble("Receiving");
+           }else{      //if state("sleeping")
+               power_consumption=4;
+               bubble("sleeping/idle");
+           }
     }
-    //code
+
+    EV<< "Power Level:" << power_consumption;
 }

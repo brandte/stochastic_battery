ab58f3f014462f9f85cb9c4e43be3f6e9c8773f1
brandte <brandte@uni-bremen.de>
2016-12-12 07:54:20 +0100

    Created the transceiver
    Jetzt tut das Netzwerk immerhin etwas. Nicht viel, aber etwas...

--- transceiver.cc
+++ transceiver.cc
@@ -6,7 +6,8 @@ using namespace omnetpp;
 class transceiver : public cSimpleModule
 {
   private:
-
+    //float power_lvl;    //The power level of the transceiver that will later be send to the battery.
+    cMessage *measuring_interval;    //This one is needed for the sender, to set the measure and send interval.
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
@@ -16,10 +17,22 @@ Define_Module(transceiver);
 
 void transceiver::initialize()
 {
-    // TODO - Generated method body
+    if (strcmp("Sender", getName()) == 0) {
+        send (new cMessage(),"gate$o");
+        measuring_interval = new cMessage("measuring interval");
+        scheduleAt(normal(1800,60),measuring_interval);
+    }
 }
 
 void transceiver::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    cGate *arrivalGate = msg->getArrivalGate();
+    if (arrivalGate==NULL){ //dann war das eine self-message und wir sind der Messpunkt
+        send (new cMessage(),"gate$o");
+        scheduleAt(simTime()+normal(1800,60),measuring_interval);
+    }else{      //dann sind wir der Empfänger und haben eine Nachricht von Außen bekommen
+        //hier haben wir jetzt eine Nachricht empfangen und müssen unseren Pegel dementsprechend anpassen.
+        //später...
+        bubble("Hurray");
+    }
 }

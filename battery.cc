f313db12118e5491a7ca1debd3a242813be8df3b
Flosch94 <Flosch@eduroam-pool12-1695.wlan.uni-bremen.de>
2016-12-15 16:14:20 +0100

    Implementing of the first statistic and analytical results.

--- battery.cc
+++ battery.cc
@@ -9,7 +9,9 @@ class battery : public cSimpleModule
     public:
         cMessage *battery_exhausted;
     private:
-
+        cOutVector stat_capacity;
+        cOutVector stat_power_level;
+        int power_level;
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
@@ -19,14 +21,18 @@ Define_Module(battery);
 
 void battery::initialize()
 {
-    //code
+    power_level=0;
+    stat_capacity.setName("Battery Capacity");
+    stat_power_level.setName("Power Consumption");
 }
 
 void battery::handleMessage(cMessage *msg)
 {
-    power_update *Power_pointer=check_and_cast<power_update *>(msg);
-    int g = Power_pointer->getPower_consum();
+    stat_power_level.record(power_level);
+    power_update *Power_pointer=check_and_cast<power_update *>(msg);            //Auslesen des Power_upates
+    power_level = Power_pointer->getPower_consum();
+    stat_power_level.record(power_level);
     bubble("Battery received!");
-    EV<< "Power Level im "<< getParentModule()->getName() << " | Batterie" <<g;
+    EV<< "Power Level im "<< getParentModule()->getName() << " | Batterie" <<power_level;
 }
 

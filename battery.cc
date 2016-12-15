29d841257dcf423d5729a73b37852529a1afcca3
brandte <brandte@uni-bremen.de>
2016-12-15 16:14:15 +0100

    Analysis function prepared

--- battery.cc
+++ battery.cc
@@ -5,8 +5,12 @@
 using namespace omnetpp;
 
 class battery : public cSimpleModule{
-public:
+    public:
         cMessage *imdead;
+    private:
+        cOutVector stat_capacity;
+        cOutVector stat_power_level;
+        int power_level;
     protected:
         virtual void initialize();
         virtual void handleMessage(cMessage *msg);
@@ -16,13 +20,16 @@ Define_Module(battery);
 
 void battery::initialize()
 {
-    // TODO - Generated method body
+    stat_capacity.setName("Battery capacity");
+    stat_power_level.setName("Power Level");
+    power_level=0;
 }
 
-void battery::handleMessage(cMessage *msg)
-{
+void battery::handleMessage(cMessage *msg){
+    stat_power_level.record(power_level);
     power_update *Power_pointer=check_and_cast<power_update *>(msg);
-    int g = Power_pointer->getPower_consum();
+    power_level = Power_pointer->getPower_consum();  //Auslesen des Power Levels
+    stat_power_level.record(power_level);
     bubble("Battery hat was empfangen");
-    EV <<"Power Level der " << getParentModule()->getName() << " Batterie " << g;
+    EV <<"Power Level der " << getParentModule()->getName() << " Batterie " << power_level;
 }

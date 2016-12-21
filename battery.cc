adb9be54409ea5ff046d070e8820ba1b2382fab4
Flosch94 <Flosch@eduroam-pool12-1363.wlan.uni-bremen.de>
2016-12-21 16:43:43 +0100

    First Beta version with implemented battery

--- battery.cc
+++ battery.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
+#include <math.h>
 #include "power_update_m.h"
 using namespace omnetpp;
 
@@ -11,9 +12,13 @@ class battery : public cSimpleModule
     private:
         cOutVector stat_capacity;
         cOutVector stat_power_level;
+        cOutVector stat_recovery;
         double power_level;
-        long double capacity;                        //Wir nutzen double - Nachkommestellen max 15 !!! Das reicht
-        double time;
+        long double float_capacity;
+        long double conversion;             //Conversion from SEU to As!! Richtige Reihenfolge beachten
+        int64_t int_capacity;
+        simtime_t time;
+        char last_activity;
 
   protected:
     virtual void initialize() override;
@@ -26,27 +31,56 @@ Define_Module(battery);
 void battery::initialize()
 {
     power_level=0;
+    last_activity='z';
     stat_capacity.setName("Battery Capacity");
     stat_power_level.setName("Power Consumption");
+    stat_recovery.setName("recovery of energy");
 
-    capacity = 2232; //[As] modified from Energizer CR2450 - 620 mAh
-    stat_capacity.record(capacity);
-    time=SIMTIME_DBL(simTime());
-
+    float_capacity = 2232; //[As] modified from Energizer CR2450 - 620 mAh
+    conversion=float_capacity/1e10;
+    int_capacity = round(float_capacity/conversion);                  //measured in --> smallest energy unit "SEU"
+    stat_capacity.record(float_capacity);
+    time=simTime();
 }
 
 void battery::handleMessage(cMessage *msg)
-{
-    double usage = power_level*(SIMTIME_DBL(simTime())-time);
-    capacity = capacity - usage;
-    stat_capacity.record(capacity);
+{   // Berechnen des Verbrauchs aus der Vorperiode
+    simtime_t delta_t = simTime()-time;
+    int64_t usage = round(delta_t.dbl()*power_level/conversion);
+    int_capacity -= usage;
+    float_capacity = int_capacity*conversion;
+    stat_capacity.record(float_capacity);
 
+    //Recovery condition
+    if(last_activity!='z'){     //Initialisierung
+        float q_0 = 0.95;
+        float g_n = 1e-10;
+        float g_c = 1e-3;
+        float p=1;
+        int64_t capacity_old = int_capacity;
+        int64_t recovery;
+
+        for (long int i=1; i <= usage; i++){
+            p=q_0*exp(-int_capacity*g_n)*exp(-i*g_c);
+            if(0.01*intuniform(0,100)<p){
+                int_capacity++;
+                recovery = int_capacity-capacity_old;
+            }
+        }
+        float_capacity = int_capacity*conversion;
+        stat_capacity.record(float_capacity);
+        stat_recovery.record(recovery);
+    }
+
+
+    // Auslesen des Memory der eingegangenen Werte
     stat_power_level.record(power_level);
-    power_update *Power_pointer=check_and_cast<power_update *>(msg);            //Auslesen des Power_upates
+    power_update *Power_pointer=check_and_cast<power_update *>(msg);            //Auslesen des Power_upates (in MikroAmpere)
     power_level = Power_pointer->getPower_consum();
     power_level = power_level/1000000;                                        //Umrechnung in Ampere
+    last_activity = Power_pointer->getCondition();
     stat_power_level.record(power_level);
-    time=SIMTIME_DBL(simTime());
+    time=simTime();
 
 }
 

c8b7d3ed6d1acc8fdc77b8538240a1ed58ec3b78
brandte <brandte@uni-bremen.de>
2016-12-22 15:18:41 +0100

    battery can acutally be empty now
    There is still a bug in the transmitter battery...

--- battery.cc
+++ battery.cc
@@ -18,6 +18,7 @@ class battery : public cSimpleModule{
         long double conversion;      //Conversion from SEU to As
         simtime_t time;
         char last_activity;
+        bool dead;
     protected:
         virtual void initialize();
         virtual void handleMessage(cMessage *msg);
@@ -32,8 +33,9 @@ void battery::initialize()
     stat_recovery.setName("How much is recovered");
     power_level=0;
     last_activity='z';
+    dead=0;
 
-    float_capacity=2232;                    //As aus Datenblatt 620mAh
+    float_capacity=108;                    //As aus Datenblatt 620mAh(alt) jetzt 30mAh
     conversion=float_capacity/1e10;                   //Normierung auf SEU.
     int_capacity=round(float_capacity/conversion);
     stat_capacity.record(float_capacity);
@@ -41,40 +43,49 @@ void battery::initialize()
 }
 
 void battery::handleMessage(cMessage *msg){
-    //Berechnen des Verbrauches in der vorangegangenen Periode.
-    simtime_t delta_t=(simTime()-time);
-    int64_t usage=round(delta_t.dbl()*power_level/conversion);
-    int_capacity-=usage;
-    float_capacity=int_capacity*conversion;
-    stat_capacity.record(float_capacity);
-
-    //Recovery
-    if(last_activity!='z'){
-        float q_0=0.95;
-        float g_n=5e-9;
-        float g_c=1e-3;
-        float p=0;      //initialisation
-        int64_t int_old_capacity =int_capacity;
-
-        for(long int i=1; i<=usage; i++){
-            p=q_0*exp(-int_capacity*g_n-i*g_c);
-            if(0.01*intuniform(0,100)<p){
-                int_capacity++;
-            }
-        }
+    if(dead==0){
+        //Berechnen des Verbrauches in der vorangegangenen Periode.
+        simtime_t delta_t=(simTime()-time);
+        int64_t usage=round(delta_t.dbl()*power_level/conversion);
+        int_capacity-=usage;
         float_capacity=int_capacity*conversion;
         stat_capacity.record(float_capacity);
-        stat_recovery.record(int_capacity-int_old_capacity);
+
+        if (int_capacity<=0){
+            dead=1;
+            imdead=new cMessage("I am dead");
+            send(imdead,"gate$o");
+        }else{
+
+            //Recovery
+            if(last_activity!='z'){
+                float q_0=0.95;
+                float g_n=1e-10;
+                float g_c=1e-5;
+                float p=0;      //initialisation
+                int64_t int_old_capacity =int_capacity;
+
+                for(long int i=1; i<=usage; i++){
+                    p=q_0*exp(-int_capacity*g_n-i*g_c);
+                    if(0.01*intuniform(0,100)<p){
+                        int_capacity++;
+                    }
+                }
+                float_capacity=int_capacity*conversion;
+                stat_capacity.record(float_capacity);
+                stat_recovery.record(int_capacity-int_old_capacity);
+            }
+
+
+
+            //Auslesen und Speicher der Eingangenen Werte.
+            stat_power_level.record(power_level);
+            power_update *Power_pointer=check_and_cast<power_update *>(msg);
+            power_level = Power_pointer->getPower_consum();  //Auslesen des Power Levels (in Mikroampere)
+            power_level=power_level/1000000;        //Jetzt auch in Ampere
+            last_activity = Power_pointer->getCurrent_activity();
+            stat_power_level.record(power_level);
+            time=simTime();
+        }
     }
-
-
-
-    //Auslesen und Speicher der Eingangenen Werte.
-    stat_power_level.record(power_level);
-    power_update *Power_pointer=check_and_cast<power_update *>(msg);
-    power_level = Power_pointer->getPower_consum();  //Auslesen des Power Levels (in Mikroampere)
-    power_level=power_level/1000000;        //Jetzt auch in Ampere
-    last_activity = Power_pointer->getCurrent_activity();
-    stat_power_level.record(power_level);
-    time=simTime();
 }

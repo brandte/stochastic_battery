0950db4ebad36a95608301d414c736f229a128ab
Flosch94 <Flosch@eduroam-pool12-710.wlan.uni-bremen.de>
2017-01-04 10:25:10 +0100

    Finished but still work with the parameters.

--- battery.cc
+++ battery.cc
@@ -19,11 +19,11 @@ class battery : public cSimpleModule
         int64_t int_capacity;
         simtime_t time;
         char last_activity;
+        bool dead;
 
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-
 };
 
 Define_Module(battery);
@@ -31,37 +31,44 @@ Define_Module(battery);
 void battery::initialize()
 {
     power_level=0;
+    dead=0;
     last_activity='z';
     stat_capacity.setName("Battery Capacity");
     stat_power_level.setName("Power Consumption");
-    stat_recovery.setName("recovery of energy");
+    stat_recovery.setName("Recovery of Energy");
 
-    float_capacity = 2232; //[As] modified from Energizer CR2450 - 620 mAh
+    float_capacity = 108; //Unit: [As] modified from Energizer CR2450 - 620 mAh | 30mAh
     conversion=float_capacity/1e10;
     int_capacity = round(float_capacity/conversion);                  //measured in --> smallest energy unit "SEU"
     stat_capacity.record(float_capacity);
     time=simTime();
 }
 
-void battery::handleMessage(cMessage *msg)
-{   // Berechnen des Verbrauchs aus der Vorperiode
+void battery::handleMessage(cMessage *msg)  // Berechnen des Verbrauchs aus der Vorperiode
+{
+if (dead==0){
     simtime_t delta_t = simTime()-time;
     int64_t usage = round(delta_t.dbl()*power_level/conversion);
     int_capacity -= usage;
     float_capacity = int_capacity*conversion;
     stat_capacity.record(float_capacity);
 
+    if(int_capacity<=0){
+        dead=1;
+        battery_exhausted = new cMessage ("Battery exhausted");
+        send(battery_exhausted, "port$o");
+    }else{
     //Recovery condition
     if(last_activity!='z'){     //Initialisierung
         float q_0 = 0.95;
-        float g_n = 1e-10;
-        float g_c = 1e-3;
-        float p=1;
+        float g_n = 3e-10;
+        float g_c = 1e-4;
+        float p=0;
         int64_t capacity_old = int_capacity;
         int64_t recovery;
 
         for (long int i=1; i <= usage; i++){
-            p=q_0*exp(-int_capacity*g_n)*exp(-i*g_c);
+            p=q_0*exp(-int_capacity*g_n-i*g_c);
             if(0.01*intuniform(0,100)<p){
                 int_capacity++;
                 recovery = int_capacity-capacity_old;
@@ -71,16 +78,14 @@ void battery::handleMessage(cMessage *msg)
         stat_capacity.record(float_capacity);
         stat_recovery.record(recovery);
     }
-
-
+   }
     // Auslesen des Memory der eingegangenen Werte
     stat_power_level.record(power_level);
-    power_update *Power_pointer=check_and_cast<power_update *>(msg);            //Auslesen des Power_upates (in MikroAmpere)
+    power_update *Power_pointer=check_and_cast<power_update *>(msg);          //Auslesen des Power_upates (in MikroAmpere)
     power_level = Power_pointer->getPower_consum();
     power_level = power_level/1000000;                                        //Umrechnung in Ampere
     last_activity = Power_pointer->getCondition();
     stat_power_level.record(power_level);
     time=simTime();
-
 }
-
+}
--- transceiver.cc
+++ transceiver.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <omnetpp.h>
-
+#include <cmath>
 #include "power_update_m.h" //msg: sends power update from transceiver to battery
 //msg: controls duration of sending time and, hence, gives information about the power used for activity
 
@@ -13,7 +13,6 @@ class transceiver : public cSimpleModule
         cMessage *battery_exhausted;
 
     private:
-        //float power_lvl;                      //The power level of the transceiver that will later be send to the battery.
         int power_consumption;                  //Power level of the Batter in micro-Ampere
         cMessage *measuring_interval_SM;        //This one is needed for the sender, to set the measure and send interval.
         cMessage *sending_time_SM;              // Sending time msg, refers to Data_laod and transmission speed.
@@ -34,31 +33,24 @@ Define_Module(transceiver);
 void transceiver::initialize(){
     dead=0;
     power_level("sleep");
-    sending_duration =sending_duration_function();                                       //Berechnung muss noch erfolgen!!!!!!!!!!!
+    sending_duration =sending_duration_function();
     int l_sending_interval = par("sending_interval");
 
     if(l_sending_interval!=0){                                                           // I'm a Sender
         if (strcmp("Sender", getParentModule()->getName())==0) {
-            //cMessage *l_data_load = new cMessage ("data_load");
-            //send(l_data_load, "transmission$o");
             sending_time_SM = new cMessage ("sending time");
             measuring_interval_SM = new cMessage("measuring interval");
-            //power_level("send");
-            //scheduleAt(simTime()+sending_duration, sending_time_SM);
-            scheduleAt(simTime()+l_sending_interval,measuring_interval_SM);
+            scheduleAt(par("sending_interval"),measuring_interval_SM);
         }else{                                                                          // I'm a Receiver
             wake_up_SM = new cMessage ("wake_up_SM_Re");
             scheduleAt(0.95*l_sending_interval,wake_up_SM);
         }
-    }
-
-}                           // RECEIVER wird an dieser Stelle nicht initalisiert, weswegen die ersten Nachricht keinen Strom verbraucht
-                            // Unschärfe wird an dieser Stelle akzeptiert
+    }                           // RECEIVER wird an dieser Stelle nicht initalisiert, weswegen die ersten Nachricht keinen Strom verbraucht
+}                               // Unschärfe wird an dieser Stelle akzeptiert
 
 void transceiver::handleMessage(cMessage *msg)
 {
     if(dead==0){
-
         if (msg==measuring_interval_SM){                                    //first self-message --> Node is Sender
             cMessage *l_data_load = new cMessage ("data_load");
             send(l_data_load, "transmission$o");
@@ -74,10 +66,10 @@ void transceiver::handleMessage(cMessage *msg)
             power_level("sleep");
         }else if(msg==wake_up_SM){
             power_level("receive");                                         //assumption of a perfect channel and no information lost
-        }else if(msg==battery_exhausted){
+        }else if(msg==battery_exhausted||strcmp(msg->getName(),("Battery exhausted"))==0){
             dead=1;
             delete msg;
-            EV<< "Battery from" << getParentModule()->getName() << "is empty";
+            EV<< "Battery of the" << getParentModule()->getName() << "is empty";
         }else{
             EV<< "ERROR+++ERROR+++ERROR+++ERROR+++ERROR+++ERROR+++ERROR+++ERROR";
             EV<< msg->getName();
@@ -90,8 +82,8 @@ void transceiver::handleMessage(cMessage *msg)
 double transceiver::sending_duration_function()
 {
     double l_sending_duration=0;
-    int l_message_length = par("message_length");
-    l_message_length=64*ceil(l_message_length/64);               //extended message length -> modulo 64
+    double l_message_length = par("message_length");
+    l_message_length=ceil(l_message_length/64)*64;               //extended message length -> modulo 64
 
     if (strcmp(par("transceiver_type"),("CC2530"))==0){             //assuming BPSK
         l_sending_duration=(l_message_length/250000);
@@ -163,7 +155,7 @@ void transceiver::power_level(std::string activity)
     } else if(activity=="receive"){
         short_activity='r';
     }else if (activity=="sleep"){
-        short_activity='z';             // i==leep
+        short_activity='z';             // z == SLEEP
     }
 
     power_update *pwrupd = new power_update();

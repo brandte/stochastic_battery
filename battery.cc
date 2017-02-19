0b81a54b212ac1d66bd1c75fafe8a93333f265c9
brandte <brandte@uni-bremen.de>
2017-02-19 14:47:52 +0100

    Comments
    A lot of comments for better tracability and for externals.

--- battery.cc
+++ battery.cc
@@ -2,27 +2,30 @@
 #include <string.h>
 #include <omnetpp.h>
 #include <cmath>
-#include "power_update_m.h"
 using namespace omnetpp;
+
+#include "power_update_m.h"         //Sends the power level and state from transceiver to battery.
 
 class battery : public cSimpleModule{
     public:
-        cMessage *imdead;
+        cMessage *imdead;           //This message tells the transceiver when the battery is empty
     private:
-        cOutVector stat_capacity;
-        cOutVector stat_power_level;
-        cOutVector stat_recovery;
-        double power_level;
-        long double float_capacity;  //Wir nutzen double, das hat ~10^15 Genauigkeit; Verhältnis von Gesamtladung zu kleinste Entladung etwa 10^11. Wir schon passen
-        int64_t int_capacity;         //measured in kee kleinste Energie Einheiten SEU smalest Energy Units
-        long double conversion;      //Conversion from SEU to As
-        simtime_t last_time;
-        char last_activity;
-        bool dead;
+        //The cOutVectors are for data collection
+        cOutVector stat_capacity;   //How much capacity is in the battery
+        cOutVector stat_recovery;   //How much energy is recovered in this step
+        //cOutVector stat_power_level;
 
-        double q_0;
-        double g_n;    //Overall decay coefficient
-        double g_c;     //How much usage can maximal be recovered.
+        double power_level;          //How much power is drained by the transceiver
+        long double float_capacity;  //The capacity as a floating point in [As]. It needs to be very big and very precise. Will mainly be used for input, as well as analysis output.
+        int64_t int_capacity;        //This one is the capacity in [SEU] (called N in the formula). All calculations will be done with this discrete one.
+        long double conversion;      //Conversion from SEU to As
+        simtime_t last_time;         //This variable will store the time, when the power consumption was changed last time.
+        char last_activity;          //This variable will store the activity that was performed by the transceiver at the last power level change.
+        bool dead;                   //When the battery is empty we change this boolean and send the information to the transceiver.
+
+        double q_0;     //Important variable for the stochastic formula. See the documentation for details. Inherited from omnetpp.ini
+        double g_n;     //Important variable for the stochastic formula. See the documentation for details. Inherited from omnetpp.ini
+        double g_c;     //Important variable for the stochastic formula. See the documentation for details. Inherited from omnetpp.ini
     protected:
         virtual void initialize();
         virtual void handleMessage(cMessage *msg);
@@ -32,67 +35,68 @@ Define_Module(battery);
 
 void battery::initialize()
 {
-    //EV <<"getScaleExp: " << omnetpp::SimTime::getScaleExp();  //Just for debug.
+    q_0=par("q_0");    //Inheritance from the omnetpp.ini
+    g_n=par("g_N");    //Inheritance from the omnetpp.ini
+    g_c=par("g_C");    //Inheritance from the omnetpp.ini
 
-    q_0=par("q_0");
-    g_n=par("g_N");    //Overall decay coefficient
-    g_c=par("g_C");    //How much usage can maximal be recovered.
+    stat_capacity.setName("Battery capacity");      //For analysis
+    //stat_power_level.setName("Power Level");      //For analysis
+    stat_recovery.setName("How much is recovered"); //For analysis
 
-    stat_capacity.setName("Battery capacity");
-    stat_power_level.setName("Power Level");
-    stat_recovery.setName("How much is recovered");
     power_level=0;
     last_activity='z';
     dead=0;
 
     double N=par("N");
-    float_capacity=par("battery_capacity");                    //As aus Datenblatt 620mAh(alt) jetzt 30mAh
-    conversion=float_capacity/N;                   //Normierung auf SEU.
+    float_capacity=par("battery_capacity");        //From onetpp.ini
+    conversion=float_capacity/N;                   //Normalization of the input to SEU.
     int_capacity=round(float_capacity/conversion);
     stat_capacity.record(float_capacity);
     last_time=simTime();
 }
 
 void battery::handleMessage(cMessage *msg){
-    if(dead==0){
-        //Berechnen des Verbrauches in der vorangegangenen Periode.
-        simtime_t delta_t=(simTime()-last_time);
-        //EV << getParentModule()->getName() << ": simtime_t delta_t: " << delta_t << "\n";
-        int64_t usage=round(power_level/conversion*delta_t.dbl());
-        //EV << getParentModule()->getName() << ": int64_t usage: " << usage;
-        int_capacity-=usage;
-        float_capacity=int_capacity*conversion;
-        stat_capacity.record(float_capacity);
+    if(dead==0){    //When we are dead (empty) we will do nothing
 
+        //calculation of the power consumption in the previous time interval.
+        simtime_t delta_t=(simTime()-last_time);
+        int64_t usage=round(power_level/conversion*delta_t.dbl());  //how much energy was consumed
+        int_capacity-=usage;                                        //update capacity
+        float_capacity=int_capacity*conversion;         //For external analysis
+        stat_capacity.record(float_capacity);           //The graph looks better when we include some redundancy.
+
+        //Recovery
         if (int_capacity<=0){
+            //When the last consumption drained to much energy, we will not restore anything.
             dead=1;
-            imdead=new cMessage("I am dead");
+            imdead=new cMessage("I am dead");   //Tell the transceiver that we are empty.
             send(imdead,"gate$o");
             EV << getParentModule()->getName() << " is empty.";
-        }else{  //Recovery
-            if(last_activity!='z'){
-                float p=0;      //Initialization
+        }else{
+            if(last_activity!='z'){         //We can not recover energy from the sleeping time, because it is considered as a constant background drain.
+                float p=0;                  //Initialization
                 int64_t int_old_capacity =int_capacity;
-
                 for(long int i=1; i<=usage; i++){
-                    p=q_0*exp(-int_capacity*g_n-i*g_c);
-                    if(0.001*intuniform(0,1000)<p){
+                    //This is the part of the code that eats a lot of time. This loop is called very often and has to execute an exponential function every time.
+                    //This is the "core element" of the stochastic battery model
+                    p=q_0*exp(-int_capacity*g_n-i*g_c);   //We calculate a probability that a SEU is recovered and we go up a step in the markov chain.
+                    if(0.001*intuniform(0,1000)<p){       //Then we draw a random number an check if the propability is hit.
                         int_capacity++;
                     }
                 }
-                float_capacity=int_capacity*conversion;
+                float_capacity=int_capacity*conversion;     //For external analysis.
                 stat_capacity.record(float_capacity);
                 stat_recovery.record(int_capacity-int_old_capacity);
             }
 
-            //Auslesen und Speicher der Eingangenen Werte.
-            stat_power_level.record(power_level);
-            power_update *Power_pointer=check_and_cast<power_update *>(msg);
-            power_level = Power_pointer->getPower_consum();  //Auslesen des Power Levels (in Mikroampere)
-            power_level=power_level/1000000;        //Jetzt auch in Ampere
-            last_activity = Power_pointer->getCurrent_activity();
-            stat_power_level.record(power_level);
-            last_time=simTime();
+            //read and store the incoming values, so we can use them in the next turn.
+            //stat_power_level.record(power_level);                               //The graph looks better when we include some redundancy.
+            power_update *Power_pointer=check_and_cast<power_update *>(msg);    //call the message
+            power_level = Power_pointer->getPower_consum();                     //read the submitted power level. In [Microampere]
+            power_level=power_level/1000000;                                    //Convert to [Ampere]
+            last_activity = Power_pointer->getCurrent_activity();               //Store the activity
+            //stat_power_level.record(power_level);
+            last_time=simTime();                                                //We need this to calculate the delta later.
         }
     }
 }
--- transceiver.cc
+++ transceiver.cc
@@ -4,73 +4,78 @@
 #include <cmath>
 using namespace omnetpp;
 
-#include "power_update_m.h"     //Sends the power level from transceiver to battery
-
+#include "power_update_m.h"     //Sends the power level and state from transceiver to battery.
 
 class transceiver : public cSimpleModule{
   public:
-    cMessage *imdead;
+    cMessage *imdead;            //This cMessage comes from the battery, when it is empty.
   private:
-    int power_consumption;      //The power level of the transceiver that will be transmitted to the battery. In Microampere
-    bool dead;
-    cMessage *measuring_interval_SM;    //This one will be a self message, to set the measure and send interval.
+    int power_consumption;       //The power level of the transceiver that will be transmitted to the battery. In microampere
+    bool dead;                   //When the power is empty this one will be set to zero.
+    cMessage *measuring_interval_SM;    //This one will be a self message (SM), to set the "measure & send"-interval.
     cMessage *sending_time_SM;          //How long does the sending take? This SM will regulate.
     cMessage *wakeup_SM;                //The SM that will wake up the receiver.
-    double sending_duration;
-    double receiver_offtime=par("receiver_offtime");
-    int transceiver_datarate=par("transceiver_datarate");
+    double sending_duration;            //How long will the sending take?  Based on data rate and message length.
+    double receiver_offtime=par("receiver_offtime");        //The receiver has a receiving window. With this value we simulate that.
+    int transceiver_datarate=par("transceiver_datarate");   //Data rate. Inherited from omnetpp.ini
 
   protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    virtual void power_level(std::string activity);// override;
-    virtual double sending_duration_func();
+    virtual void power_level(std::string activity);        //This one will prepare and send the power_update message.
+    virtual double sending_duration_func();                //This one gives us the sending duration
 };
 
 Define_Module(transceiver);
 
 void transceiver::initialize(){
-    dead=0;
-    power_level("sleep");
+    dead=0;                     //We are alive
+    power_level("sleep");       //Everyone starts sleeping
     int l_sending_interval=par("sending_interval");
     sending_duration=sending_duration_func();
 
-    if (l_sending_interval!=0){
+    if (l_sending_interval!=0){         //When this is zero we do nothing.
         if (strcmp("Sender", getParentModule()->getName()) == 0){
-            sending_time_SM = new cMessage("sending time SM");
-            measuring_interval_SM = new cMessage("measuring interval");
-            scheduleAt(par("sending_interval"),measuring_interval_SM);
-        }else{  //ich bin der Receiver
-            wakeup_SM=new cMessage("wake up SM for Receiver");
-            scheduleAt(receiver_offtime*l_sending_interval,wakeup_SM);
+            sending_time_SM = new cMessage("sending time SM");              //We initialize the SM
+            measuring_interval_SM = new cMessage("measuring interval");     //same here.
+            scheduleAt(par("sending_interval"),measuring_interval_SM);      //The sender interval is started. When this SM will arrive the first message will be send.
+        }else{  //receiver
+            wakeup_SM=new cMessage("wake up SM for Receiver");              //SM is initialized
+            scheduleAt(receiver_offtime*l_sending_interval,wakeup_SM);      //and send. The receiver will be active for a while, before it receives the message from the sender.
         }
     }
 }
 
 void transceiver::handleMessage(cMessage *msg){
     if(dead==0){
-        if (msg==measuring_interval_SM){ //dann war das eine self-message und wir sind der Messpunkt
-            cMessage *l_data_load = new cMessage("data_load");
+        //when dead==0 the battery is empty an we do nothing at all.
+        if (msg==measuring_interval_SM){
+            //when this SM hits, we are the sender and it is time to send a message to the receiver.
+            cMessage *l_data_load = new cMessage("data_load");      //This is the message that is send to the receiver. It is empty, because in this scenario we don't care.
             send (l_data_load,"transmission$o");
-            power_level("send");
-            scheduleAt(simTime()+sending_duration,sending_time_SM);
-            scheduleAt(simTime()+par("sending_interval"),measuring_interval_SM);
+            power_level("send");                                    //Call of the function, to interact with the battery.
+            scheduleAt(simTime()+sending_duration,sending_time_SM);                 //This self message determines when the status will be set back to sleep
+            scheduleAt(simTime()+par("sending_interval"),measuring_interval_SM);    //This SM will activate the next sending process.
         }else if(msg==sending_time_SM){
+            //when the sending time is over, we tell the battery that we go back to sleep power level.
             power_level("sleep");
-        }else if(strcmp(msg->getName(),"data_load")==0){      //dann sind wir der Empf‰nger und haben eine Nachricht von Auﬂen bekommen
-            //EV << getParentModule()->getName() << " hat eine Nachricht empfangen.";
-            power_level("sleep");
+        }else if(strcmp(msg->getName(),"data_load")==0){
+            //When this comes in, we are the receiver and received a message from outside.
+            //EV << getParentModule()->getName() << " has received a message.";
+            power_level("sleep");               //We do not consider the time that it takes to receive and process the message. It should be much smaller than the receiving window so it can be neglected.
             double l_sending_interval=par("sending_interval");
-            scheduleAt(simTime()+receiver_offtime*l_sending_interval,wakeup_SM);
+            scheduleAt(simTime()+receiver_offtime*l_sending_interval,wakeup_SM);    //This SM wakes us, when the receiving window should open again.
             delete msg;
         }else if(msg==wakeup_SM){
-            power_level("receive");         //Wir nehmen einen perfekten Kanal an. Keine Nachricht geht verloren, daher keine weiteren Fallunterscheidungen.
-        }else if(msg==imdead||strcmp(msg->getName(),"I am dead")==0){  //Battery is empty.
-            dead=1;
+            //The receiving window opens again. We assume a perfect channel so that a message will arrive in any case to close the window.
+            power_level("receive");
+        }else if(msg==imdead||strcmp(msg->getName(),"I am dead")==0){
+            //When the battery is empty it sends this message.
+            dead=1; //In the next call of handleMessage nothing will happen. As in every future call.
             EV << getParentModule()->getName() << "s battery is empty.";
             delete msg;
-        }else{
-            EV << "Irgendwas laeuft hier falsch: " << getParentModule()->getName() << "\n";
+        }else{      //This case should not be occure.
+            EV << "Something is wrong here: " << getParentModule()->getName() << "\n";
             EV << msg->getName();
             delete msg;
         }
@@ -78,6 +83,7 @@ void transceiver::handleMessage(cMessage *msg){
 }
 
 double transceiver::sending_duration_func(){
+    //This function calculates how long the sender will be sending depending on the given parameters from the omnetpp.ini
     double l_sending_duration;
     double l_message_length=par("message_length");
     l_message_length=ceil(l_message_length/64)*64;
@@ -98,6 +104,9 @@ double transceiver::sending_duration_func(){
 }
 
 void transceiver::power_level(std::string activity){
+    //When you give this function an activity ("send", "receive", "sleep") it will tell the battery how much power is needed for that.
+    //It is called when the activity starts and does not say when it ends. It will just send the next state.
+    //The consumption rates are based on the input from the omnetpp.ini
 
     if (strcmp(par("transceiver_type"),("CC2530"))==0){
         if (activity=="send"){
@@ -133,7 +142,7 @@ void transceiver::power_level(std::string activity){
         }else{      // Sleeping
             power_consumption=4;
         }
-    }else{
+    }else{      //nothing is given, we use the manually set values.
         if (activity=="send"){
             power_consumption=par("transceiver_energy_sending");
         }else if(activity=="receive"){
@@ -142,13 +151,14 @@ void transceiver::power_level(std::string activity){
             power_consumption=par("transceiver_energy_sleeping");
         }
     }
-    char short_activity='e';  //e for error; We need a char, because strings are not allowed in messages.
+    //The message does not allow strings so we have to set a char.
+    char short_activity='e';        //Initialized with e for error, because it should in any case be changed.
     if (activity=="send"){short_activity='s';}
     else if(activity=="receive"){short_activity='r';}
-    else if(activity=="sleep"){short_activity='z';}   //z for sleep.
+    else if(activity=="sleep"){short_activity='z';}
 
     power_update *pwr_upd=new power_update();
     pwr_upd->setPower_consum(power_consumption);
     pwr_upd->setCurrent_activity(short_activity);
-    send(pwr_upd,"battery_connection$o");
+    send(pwr_upd,"battery_connection$o");       //And here the message goes...
 }
